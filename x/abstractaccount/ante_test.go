5983e85ab8d346e540dcf74feb2082e296f23de4
larry <26318510+larry0x@users.noreply.github.com>
2023-05-29 14:16:33 +0100

    add test for BeforeTx

--- x/abstractaccount/ante_test.go
+++ x/abstractaccount/ante_test.go
@@ -1,18 +1,29 @@
 package abstractaccount_test
 
 import (
+	"fmt"
 	"testing"
+	"time"
 
 	tmproto "github.com/cometbft/cometbft/proto/tendermint/types"
 	"github.com/stretchr/testify/require"
 
 	"github.com/cosmos/cosmos-sdk/crypto/keyring"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/tx/signing"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 
 	simapptesting "github.com/larry0x/abstract-account/simapp/testing"
 	"github.com/larry0x/abstract-account/x/abstractaccount"
+	"github.com/larry0x/abstract-account/x/abstractaccount/testdata"
 	"github.com/larry0x/abstract-account/x/abstractaccount/types"
+)
+
+const (
+	mockChainID = "dev-1"
+	mockAccNum  = uint64(12345)
+	mockSeq     = uint64(88888)
+	signMode    = signing.SignMode_SIGN_MODE_DIRECT
 )
 
 func TestIsAbstractAccountTx(t *testing.T) {
@@ -22,7 +33,7 @@ func TestIsAbstractAccountTx(t *testing.T) {
 		keybase = keyring.NewInMemory(app.Codec())
 	)
 
-	// we create two mock accounts: 1 a BaseAccount, 2 an AbstractAccount
+	// we create two mock accounts: 1, a BaseAccount, 2, an AbstractAccount
 	acc1, err := makeMockAccount(keybase, "test1")
 	require.NoError(t, err)
 
@@ -38,6 +49,20 @@ func TestIsAbstractAccountTx(t *testing.T) {
 		expIs bool
 	}{
 		{
+			desc: "tx has one signer and it is an AbstractAccount",
+			msgs: []sdk.Msg{
+				banktypes.NewMsgSend(acc2.GetAddress(), acc1.GetAddress(), sdk.NewCoins()),
+			},
+			expIs: true,
+		},
+		{
+			desc: "tx has one signer but it's not an AbstractAccount",
+			msgs: []sdk.Msg{
+				banktypes.NewMsgSend(acc1.GetAddress(), acc2.GetAddress(), sdk.NewCoins()),
+			},
+			expIs: false,
+		},
+		{
 			desc: "tx has more than one signers",
 			msgs: []sdk.Msg{
 				banktypes.NewMsgSend(acc1.GetAddress(), acc2.GetAddress(), sdk.NewCoins()),
@@ -45,24 +70,8 @@ func TestIsAbstractAccountTx(t *testing.T) {
 			},
 			expIs: false,
 		},
-		{
-			desc: "tx has one signer but it's not an AbstractAccount",
-			msgs: []sdk.Msg{
-				banktypes.NewMsgSend(acc1.GetAddress(), acc2.GetAddress(), sdk.NewCoins()),
-				banktypes.NewMsgSend(acc1.GetAddress(), acc2.GetAddress(), sdk.NewCoins()),
-			},
-			expIs: false,
-		},
-		{
-			desc: "tx has one signer and it is an AbstractAccount",
-			msgs: []sdk.Msg{
-				banktypes.NewMsgSend(acc2.GetAddress(), acc1.GetAddress(), sdk.NewCoins()),
-				banktypes.NewMsgSend(acc2.GetAddress(), acc1.GetAddress(), sdk.NewCoins()),
-			},
-			expIs: true,
-		},
 	} {
-		sigTx, err := prepareTx(ctx, app, keybase, tc.msgs)
+		sigTx, err := prepareTx(ctx, app, tc.msgs)
 		require.NoError(t, err)
 
 		is, _, _, err := abstractaccount.IsAbstractAccountTx(ctx, sigTx, app.AccountKeeper)
@@ -72,7 +81,129 @@ func TestIsAbstractAccountTx(t *testing.T) {
 }
 
 func TestBeforeTx(t *testing.T) {
-	// TODO
+	var (
+		app     = simapptesting.MakeSimpleMockApp()
+		keybase = keyring.NewInMemory(app.Codec())
+	)
+
+	ctx := app.NewContext(false, tmproto.Header{
+		// must specify a time, otherwise will get this error:
+		// panic: Block (unix) time must never be empty or negative
+		Time: time.Now(),
+
+		// make sure to use our mockChainID, otherwise sig verification will fail
+		ChainID: mockChainID,
+	})
+
+	// create two mock accounts
+	acc1, err := makeMockAccount(keybase, "test1")
+	require.NoError(t, err)
+
+	acc2, err := makeMockAccount(keybase, "test2")
+	require.NoError(t, err)
+
+	// register the account
+	absAcc, err := storeCodeAndRegisterAccount(
+		ctx,
+		app,
+		// use the pubkey of acc1 as the AbstractAccount's pubkey
+		acc1.GetAddress(),
+		testdata.AccountWasm,
+		&AccountInitMsg{PubKey: acc1.GetPubKey().Bytes()},
+		sdk.NewCoins(),
+	)
+	require.NoError(t, err)
+
+	// change the AbstractAccount's account number and sequence to some non-zero
+	// numbers to make the tests harder
+	app.AccountKeeper.RemoveAccount(ctx, absAcc)
+	absAcc.SetAccountNumber(mockAccNum)
+	absAcc.SetSequence(mockSeq)
+	app.AccountKeeper.SetAccount(ctx, absAcc)
+
+	for _, tc := range []struct {
+		desc     string
+		signWith string
+		chainID  string
+		accNum   uint64
+		seq      uint64
+		expOk    bool
+	}{
+		{
+			desc:     "tx signed with the correct key",
+			signWith: "test1",
+			chainID:  mockChainID,
+			accNum:   mockAccNum,
+			seq:      mockSeq,
+			expOk:    true,
+		},
+		{
+			desc:     "tx signed with an incorrect key",
+			signWith: "test2",
+			chainID:  mockChainID,
+			accNum:   mockAccNum,
+			seq:      mockSeq,
+			expOk:    false,
+		},
+		{
+			desc:     "tx signed with an incorrect chain id",
+			signWith: "test1",
+			chainID:  "wrong-chain-id",
+			accNum:   mockAccNum,
+			seq:      mockSeq,
+			expOk:    false,
+		},
+		{
+			desc:     "tx signed with an incorrect account number",
+			signWith: "test1",
+			chainID:  mockChainID,
+			accNum:   4524455,
+			seq:      mockSeq,
+			expOk:    false,
+		},
+		{
+			desc:     "tx signed with an incorrect sequence",
+			signWith: "test1",
+			chainID:  mockChainID,
+			accNum:   mockAccNum,
+			seq:      5786786,
+			expOk:    false,
+		},
+	} {
+		fmt.Println("running test case: ", tc.desc)
+
+		msg := banktypes.NewMsgSend(absAcc.GetAddress(), acc2.GetAddress(), sdk.NewCoins())
+
+		tx, err := prepareTx2(
+			ctx,
+			app,
+			[]sdk.Msg{msg},
+			keybase,
+			tc.signWith,
+			absAcc,
+			tc.chainID,
+			tc.accNum,
+			tc.seq,
+		)
+		require.NoError(t, err)
+
+		decorator := makeBeforeTxDecorator(app)
+		_, err = decorator.AnteHandle(ctx, tx, false, anteTerminator)
+
+		if tc.expOk {
+			require.NoError(t, err)
+
+			// the signer address should have been stored for use by the PostHandler
+			signerAddr := app.AbstractAccountKeeper.GetSignerAddress(ctx)
+			require.Equal(t, absAcc.GetAddress(), signerAddr)
+
+			// delete the stored signer address so that we start from a clean state
+			// for the next test case
+			app.AbstractAccountKeeper.DeleteSignerAddress(ctx)
+		} else {
+			require.Error(t, err)
+		}
+	}
 }
 
 func TestAfterTx(t *testing.T) {
--- x/abstractaccount/module_test.go
+++ x/abstractaccount/module_test.go
@@ -8,10 +8,8 @@ import (
 	"errors"
 	"fmt"
 
-	clienttx "github.com/cosmos/cosmos-sdk/client/tx"
 	"github.com/cosmos/cosmos-sdk/crypto/hd"
 	"github.com/cosmos/cosmos-sdk/crypto/keyring"
-	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/tx/signing"
 	authsigning "github.com/cosmos/cosmos-sdk/x/auth/signing"
@@ -24,17 +22,23 @@ import (
 	"github.com/larry0x/abstract-account/x/abstractaccount/types"
 )
 
-const mockChainID = "dev-1"
-
-func makeBeforeTxDecoratorForTesting(app *simapp.SimApp) abstractaccount.BeforeTxDecorator {
-	return abstractaccount.NewBeforeTxDecorator(
-		app.AbstractAccountKeeper,
-		app.AccountKeeper,
-		app.TxConfig().SignModeHandler(),
-	)
+type AccountInitMsg struct {
+	PubKey []byte `json:"pubkey"`
 }
 
-func makeAfterTxDecoratorForTesting(app *simapp.SimApp) abstractaccount.AfterTxDecorator {
+func anteTerminator(ctx sdk.Context, tx sdk.Tx, simulate bool) (sdk.Context, error) {
+	return ctx, nil
+}
+
+func postTerminator(ctx sdk.Context, tx sdk.Tx, simulate, success bool) (sdk.Context, error) {
+	return ctx, nil
+}
+
+func makeBeforeTxDecorator(app *simapp.SimApp) abstractaccount.BeforeTxDecorator {
+	return abstractaccount.NewBeforeTxDecorator(app.AbstractAccountKeeper, app.AccountKeeper, app.TxConfig().SignModeHandler())
+}
+
+func makeAfterTxDecorator(app *simapp.SimApp) abstractaccount.AfterTxDecorator {
 	return abstractaccount.NewAfterTxDecorator(app.AbstractAccountKeeper)
 }
 
@@ -60,9 +64,7 @@ func makeMockAccount(keybase keyring.Keyring, uid string) (authtypes.AccountI, e
 
 // Logics in this function is mostly copied from:
 // cosmos/cosmos-sdk/x/auth/ante/testutil_test.go/CreateTestTx
-func prepareTx(ctx sdk.Context, app *simapp.SimApp, keybase keyring.Keyring, msgs []sdk.Msg) (authsigning.Tx, error) {
-	signMode := app.TxConfig().SignModeHandler().DefaultMode()
-
+func prepareTx(ctx sdk.Context, app *simapp.SimApp, msgs []sdk.Msg) (authsigning.Tx, error) {
 	txBuilder := app.TxConfig().NewTxBuilder()
 
 	if err := txBuilder.SetMsgs(msgs...); err != nil {
@@ -80,67 +82,17 @@ func prepareTx(ctx sdk.Context, app *simapp.SimApp, keybase keyring.Keyring, msg
 		signerAccs = append(signerAccs, signerAcc)
 	}
 
-	// round 1: set empty signatures
+	// set an empty signature for each signer
+	// for this test we don't need valid signatures
 	sigs := []signing.SignatureV2{}
 	for _, signerAcc := range signerAccs {
-		sigData := &signing.SingleSignatureData{
-			SignMode:  signMode,
-			Signature: nil,
-		}
-
 		sig := signing.SignatureV2{
-			PubKey:   signerAcc.GetPubKey(),
-			Data:     sigData,
+			PubKey: signerAcc.GetPubKey(),
+			Data: &signing.SingleSignatureData{
+				SignMode:  signMode,
+				Signature: nil,
+			},
 			Sequence: signerAcc.GetSequence(),
-		}
-
-		sigs = append(sigs, sig)
-	}
-
-	if err := txBuilder.SetSignatures(sigs...); err != nil {
-		return nil, err
-	}
-
-	// round 2: all signer infos are set, each signer can sign
-	sigs = []signing.SignatureV2{}
-	for _, signerAcc := range signerAccs {
-		r, err := keybase.KeyByAddress(signerAcc.GetAddress())
-		if err != nil {
-			return nil, err
-		}
-
-		rl := r.GetLocal()
-		if rl == nil {
-			return nil, keyring.ErrPrivKeyExtr
-		}
-
-		if rl.PrivKey == nil {
-			return nil, errors.New("private key is not available")
-		}
-
-		privKey, ok := rl.PrivKey.GetCachedValue().(cryptotypes.PrivKey)
-		if !ok {
-			return nil, errors.New("unable to cast any to cryptotypes.PrivKey")
-		}
-
-		signerData := authsigning.SignerData{
-			Address:       signerAcc.GetAddress().String(),
-			ChainID:       mockChainID,
-			AccountNumber: signerAcc.GetAccountNumber(),
-			Sequence:      signerAcc.GetSequence(),
-			PubKey:        signerAcc.GetPubKey(),
-		}
-
-		sig, err := clienttx.SignWithPrivKey(
-			signMode,
-			signerData,
-			txBuilder,
-			privKey,
-			app.TxConfig(),
-			signerAcc.GetSequence(),
-		)
-		if err != nil {
-			return nil, err
 		}
 
 		sigs = append(sigs, sig)
@@ -153,23 +105,89 @@ func prepareTx(ctx sdk.Context, app *simapp.SimApp, keybase keyring.Keyring, msg
 	return txBuilder.GetTx(), nil
 }
 
+// Similar to prepareTx, this function also takes a slice of messages, compose
+// a tx, and sign it. The differences are that
+//  1. this function only works for txs that has exactly one signer, and this
+//     signer is an AbstractAccount
+//  2. this function allows altering the signer account, chain-id, sequence, and
+//     account-number
+func prepareTx2(
+	ctx sdk.Context, app *simapp.SimApp, msgs []sdk.Msg,
+	keybase keyring.Keyring, keyName string,
+	absAcc *types.AbstractAccount, chainID string, accNum, seq uint64,
+) (authsigning.Tx, error) {
+	txBuilder := app.TxConfig().NewTxBuilder()
+
+	if err := txBuilder.SetMsgs(msgs...); err != nil {
+		return nil, err
+	}
+
+	// round 1: set empty signature
+	sig := signing.SignatureV2{
+		PubKey: absAcc.GetPubKey(), // NilPubKey
+		Data: &signing.SingleSignatureData{
+			SignMode:  signMode,
+			Signature: nil, // empty
+		},
+		Sequence: seq,
+	}
+
+	if err := txBuilder.SetSignatures(sig); err != nil {
+		return nil, err
+	}
+
+	// round 2: sign the tx
+	signerData := authsigning.SignerData{
+		Address:       absAcc.GetAddress().String(),
+		ChainID:       chainID,
+		AccountNumber: accNum,
+		Sequence:      seq,
+		PubKey:        absAcc.GetPubKey(), // NilPubKey
+	}
+
+	signBytes, err := app.TxConfig().SignModeHandler().GetSignBytes(signMode, signerData, txBuilder.GetTx())
+	if err != nil {
+		return nil, err
+	}
+
+	sigBytes, _, err := keybase.Sign(keyName, signBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	sig = signing.SignatureV2{
+		PubKey: absAcc.GetPubKey(), // NilPubKey
+		Data: &signing.SingleSignatureData{
+			SignMode:  signMode,
+			Signature: sigBytes,
+		},
+		Sequence: seq,
+	}
+
+	if err := txBuilder.SetSignatures(sig); err != nil {
+		return nil, err
+	}
+
+	return txBuilder.GetTx(), nil
+}
+
 func storeCodeAndRegisterAccount(
 	ctx sdk.Context, app *simapp.SimApp, senderAddr sdk.AccAddress,
 	bytecode []byte, msg interface{}, funds sdk.Coins,
-) (codeID uint64, contractAddr sdk.Address, err error) {
+) (*types.AbstractAccount, error) {
 	k := app.AbstractAccountKeeper
 	msgServer := keeper.NewMsgServerImpl(k)
 
 	// store code
-	codeID, _, err = k.ContractKeeper().Create(ctx, senderAddr, testdata.AccountWasm, nil)
+	codeID, _, err := k.ContractKeeper().Create(ctx, senderAddr, testdata.AccountWasm, nil)
 	if err != nil {
-		return 0, nil, err
+		return nil, err
 	}
 
 	// prepare the contract instantiate msg
 	msgBytes, err := json.Marshal(msg)
 	if err != nil {
-		return 0, nil, err
+		return nil, err
 	}
 
 	// register the account
@@ -178,15 +196,26 @@ func storeCodeAndRegisterAccount(
 		CodeID: codeID,
 		Msg:    msgBytes,
 		Funds:  funds,
+		Salt:   []byte("henlo"),
 	})
 	if err != nil {
-		return 0, nil, err
+		return nil, err
 	}
 
-	contractAddr, err = sdk.AccAddressFromBech32(res.Address)
+	contractAddr, err := sdk.AccAddressFromBech32(res.Address)
 	if err != nil {
-		return 0, nil, err
+		return nil, err
 	}
 
-	return codeID, contractAddr, nil
+	acc := app.AccountKeeper.GetAccount(ctx, contractAddr)
+	if acc == nil {
+		return nil, errors.New("account not found")
+	}
+
+	abcAcc, ok := acc.(*types.AbstractAccount)
+	if !ok {
+		return nil, errors.New("account is not an AbstractAccount")
+	}
+
+	return abcAcc, nil
 }
