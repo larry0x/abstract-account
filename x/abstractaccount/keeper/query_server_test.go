82947a5f187deb134b1742282e8fab754d474c20
larry <26318510+larry0x@users.noreply.github.com>
2023-06-10 17:21:03 +0100

    add test for queryServer

--- x/abstractaccount/keeper/query_server_test.go
+++ x/abstractaccount/keeper/query_server_test.go
@@ -1,3 +1,20 @@
 package keeper_test
 
-// TODO
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/larry0x/abstract-account/x/abstractaccount/keeper"
+	"github.com/larry0x/abstract-account/x/abstractaccount/types"
+)
+
+func TestQueryParams(t *testing.T) {
+	ctx, app := setupGenesisTest()
+
+	queryServer := keeper.NewQueryServerImpl(app.AbstractAccountKeeper)
+
+	res, err := queryServer.Params(ctx, &types.QueryParamsRequest{})
+	require.NoError(t, err)
+	require.Equal(t, mockParams, res.Params)
+}
